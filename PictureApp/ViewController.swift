e914dbe8c14344f80d320da388a81bb68fee4b9a
Varun Jit Singh <varunjitsingh6410@gmail.com>
2020-07-13 15:33:33 -0700

    Starter code

---
+++ PictureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureApp
+//
+//  Created by Varun Jit Singh on 7/13/20.
+//  Copyright © 2020 ThurgoodMarshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
